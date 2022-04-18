46b533b92ed73391d2a8dd1453e3d985a73c627b
ThisGameZR <timezazare@gmail.com>
2022-04-18 09:13:37 +0700

    Sidebar Transition, onClickOutside Hook

--- 7_Frontend/memt/src/hooks/useClickOutside.jsx
+++ 7_Frontend/memt/src/hooks/useClickOutside.jsx
@@ -3,14 +3,14 @@ import React, { useRef, useEffect } from "react";
 /**
  * Hook that alerts clicks outside of the passed ref
  */
-function useOutsideAlerter(ref) {
+function useOutsideClicker(ref, onClickOutside) {
   useEffect(() => {
     /**
      * Alert if clicked on outside of element
      */
     function handleClickOutside(event) {
       if (ref.current && !ref.current.contains(event.target)) {
-        alert("You clicked outside of me!");
+        onClickOutside();
       }
     }
     // Bind the event listener
@@ -25,9 +25,14 @@ function useOutsideAlerter(ref) {
 /**
  * Component that alerts if you click outside of it
  */
-export default function OutsideAlerter(props) {
+export default function ClickOutside(props) {
   const wrapperRef = useRef(null);
-  useOutsideAlerter(wrapperRef);
+  useOutsideClicker(wrapperRef, props.onClickOutside);
 
   return <div ref={wrapperRef}>{props.children}</div>;
 }
+
+/// Example:
+// <ClickOutside onClickOutside={() => alert("Clicked outside")}>
+//   <div>Click inside</div>
+// </ClickOutside>
--- 7_Frontend/memt/src/layouts/Sidebar.jsx
+++ 7_Frontend/memt/src/layouts/Sidebar.jsx
@@ -3,45 +3,47 @@ import { SidebarToggle } from "../contexts/SidebarContext";
 import { AiFillHome, AiFillBell, AiFillSetting } from "react-icons/ai";
 import { IoIosStats } from "react-icons/io";
 import { GoPencil } from "react-icons/go";
+import ClickOutside from "../hooks/useClickOutside";
 
 export default function Sidebar() {
   const { sidebarToggle, setSidebarToggle } = useContext(SidebarToggle);
 
-  if (!sidebarToggle) return <></>;
   return (
-    <div className="sidebar">
-      <div
-        className="sidebar-header"
-        onClick={() => {
-          setSidebarToggle(false);
-        }}
-      >
-        &#10006;
+    <ClickOutside onClickOutside={() => setSidebarToggle(false)}>
+      <div className={sidebarToggle ? "sidebar sidebar-active" : "sidebar sidebar-inactive"}>
+        <div
+          className="sidebar-header"
+          onClick={() => {
+            setSidebarToggle(false);
+          }}
+        >
+          &#10006;
+        </div>
+        <div className="sidebar-body">
+          <ul className="sidebar-body-menu">
+            <li className="sidebar-body-menu-list">
+              <AiFillHome />
+              Home
+            </li>
+            <li className="sidebar-body-menu-list">
+              <IoIosStats />
+              Report
+            </li>
+            <li className="sidebar-body-menu-list">
+              <GoPencil />
+              Profile
+            </li>
+            <li className="sidebar-body-menu-list">
+              <AiFillBell />
+              Notification
+            </li>
+            <li className="sidebar-body-menu-list">
+              <AiFillSetting />
+              Setting
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className="sidebar-body">
-        <ul className="sidebar-body-menu">
-          <li className="sidebar-body-menu-list">
-            <AiFillHome />
-            Home
-          </li>
-          <li className="sidebar-body-menu-list">
-            <IoIosStats />
-            Report
-          </li>
-          <li className="sidebar-body-menu-list">
-            <GoPencil />
-            Profile
-          </li>
-          <li className="sidebar-body-menu-list">
-            <AiFillBell />
-            Notification
-          </li>
-          <li className="sidebar-body-menu-list">
-            <AiFillSetting />
-            Setting
-          </li>
-        </ul>
-      </div>
-    </div>
+    </ClickOutside>
   );
 }
